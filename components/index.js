c31608ddcc6faef7884f151a2f32a49dbbd93295
Tymek <tymek.morawiec@gmail.com>
2023-01-02 00:30:10 +0100

    i put up all of the content so far

--- components/index.js
+++ components/index.js
@@ -1,5 +1,18 @@
-const sideNavHandler = document.querySelector(".dropdown");
+const hamburgerMenu = document.getElementById("hamburger__menu");
+
 const sideNav = document.querySelector(".sideNav");
-sideNavHandler.addEventListener("click", () => {
+
+hamburgerMenu.addEventListener("click", () => {
+  window.scrollTo(0, 0);
+  hamburgerMenu.classList.toggle("open");
+  document.body.classList.toggle("disable__scrolling");
   sideNav.classList.toggle("hidden");
+});
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 900) {
+    sideNav.classList.add("hidden");
+    document.body.classList.remove("disable__scrolling");
+    hamburgerMenu.classList.remove("open");
+  }
 });
