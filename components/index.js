7199e7a48876423807836ba6f2b29b24603912ba
Tymek <tymek.morawiec@gmail.com>
2023-01-02 17:05:00 +0100

    first draft

--- components/index.js
+++ components/index.js
@@ -1,7 +1,12 @@
 const hamburgerMenu = document.getElementById("hamburger__menu");
 
 const sideNav = document.querySelector(".sideNav");
-
+const main = document.querySelector("main");
+const logo = document.getElementById("logo");
+const sideNavBlogs = document
+  .querySelector(".sideNav__blog")
+  .querySelectorAll("li");
+console.log(sideNavBlogs);
 hamburgerMenu.addEventListener("click", () => {
   window.scrollTo(0, 0);
   hamburgerMenu.classList.toggle("open");
@@ -16,3 +21,25 @@ window.addEventListener("resize", () => {
     hamburgerMenu.classList.remove("open");
   }
 });
+const removeChildNodes = (parent) => {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+};
+const render = (i) => {
+  removeChildNodes(main);
+  const clonePage = document.getElementById(`page${i}`).content.cloneNode(true);
+  main.appendChild(clonePage);
+};
+
+for (let i = 0; i < sideNavBlogs.length; i++) {
+  sideNavBlogs[i].addEventListener("click", () => {
+    render(i + 1);
+  });
+}
+logo.addEventListener("click", () => {
+  render(0);
+});
+window.onload = () => {
+  render(0);
+};
